4bc891c25d38b0b07a5d42bb167663959137cc79
Almaz Shaiakhmetov <sh.almaz87@gmail.com>
2021-11-09 20:13:41 +0500

    3rd homework

--- lesson_3/hw_3_2.py
+++ lesson_3/hw_3_2.py
@@ -1,5 +1,14 @@
 # 2. Реализовать функцию, принимающую несколько параметров, описывающих данные пользователя:
-# имя, фамилия, год рождения, город проживания, email, телефон. Функция должна принимать параметры
+# имя, фамилия, год рождения, город проживания, email, email. Функция должна принимать параметры
 # как именованные аргументы. Реализовать вывод данных о пользователе одной строкой.
-def user_data():
-    name
+def user_data(name, surname, date, city, email, phone):
+    print(f"{name} {surname} {date} {city} {email} {phone}")
+
+
+name = input("Введите имя: ")
+surname = input("Введите фамилию: ")
+date = input("Введите год рождения: ")
+city = input("Введите город проживания: ")
+email = input("Введите email: ")
+phone = input("Введите телефон: ")
+user_data(name=name, surname=surname, date=date, city=city, email=email, phone=phone)
